e5243c461b8b373b05766be58d14064642934c19
Nicholas Oxford <51415676+nicholasoxford@users.noreply.github.com>
2020-06-12 16:51:38 -0400

    /clima-node/

--- clima-node/src/climaCall.js
+++ clima-node/src/climaCall.js
@@ -1,11 +1,20 @@
 import { loadJson, saveJSON } from "./jsonCRUD";
+import chalk from "chalk";
+const ProgressBar = require("./ProgressBar");
+
 var axios = require("axios");
 let listofTemps = [];
 export async function get_clima(api) {
   const apikey = api;
   const climaJSON = loadJson("clima-node/src/json/longLangByCity.json");
+  const Bar = new ProgressBar();
+
   let c;
-  for (c = 0; c < 50; c++) {
+  let apiLimit = 50;
+  Bar.init(apiLimit--);
+  console.log(chalk.green.bold("Downloading wether data."));
+  for (c = 0; c < apiLimit; c++) {
+    Bar.update(c);
     await axios
       .get("https://api.climacell.co/v3/weather/realtime?lat=" + climaJSON[c][1].toString() + "&lon=" + climaJSON[c][0].toString() + "&unit_system=us&fields=temp%2Cfeels_like&apikey=" + apikey)
       .then((response) => {
